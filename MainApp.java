181c3327563c2c1ffb0b46003280c31dfb49a691
VijayEpam <45033250+VijayEpam@users.noreply.github.com>
2018-11-14 16:51:27 +0530

    Create MainApp.java

---
+++ MainApp.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class MainApp{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your choice: \n 1.ICM \n 2.FCM \n 3.ICF");
+int choice=sc.nextInt();
+switch(choice){
+case 1:sc.nextInt()
+
+}
+
+
+}
